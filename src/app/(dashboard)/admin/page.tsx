3ababbe5ed3679c301240b442aaca76fdeefbdbf
galangriarulhaq <galangrz337@gmail.com>
2025-02-14 10:34:39 +0700

    Crate Component Count Chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,16 +1,29 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="flex flex-col p-4 gap-4 md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User Cards */}
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* Middle Chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom Chart */}
+        <div className=""></div>
       </div>
       {/* Right */}
       <div className="w-full lg:w-1/3">R</div>
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] overflow-auto scrollbar-hide">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 p-4">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-sans">SchollKu</span>
+          <span className="hidden lg:block text:lg font-semibold">SchollKu</span>
         </Link>
         <Menu />
       </div>
