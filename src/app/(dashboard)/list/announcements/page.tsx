ecafb642a1a81031d95a40e74b5b2ff7872370c1
galangriarulhaq <galangrz337@gmail.com>
2025-02-25 21:40:26 +0700

    Create Announcements Page

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,17 +1,15 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { eventsData, role } from "@/lib/data";
+import { announcementsData, eventsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Event = {
+type Announcement = {
   id: number;
   title: string;
   class: string;
   date: string;
-  startTime: string;
-  endTime: string;
 };
 
 const columns = [
@@ -29,29 +27,17 @@ const columns = [
     className: "hidden md:table-cell",
   },
   {
-    header: "Start Time",
-    accessor: "startTime",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "End Time",
-    accessor: "endTime",
-    className: "hidden md:table-cell",
-  },
-  {
     header: "Actions",
     accessor: "actions",
   },
 ];
 
-const EventListPage = () => {
-  const renderRow = (item: Event) => (
+const AnnouncementListPage = () => {
+  const renderRow = (item: Announcement) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-sPurpleLight">
       <td className="flex items-center gap-4 p-4 font-semibold">{item.title}</td>
       <td>{item.class}</td>
       <td className="hidden md:table-cell">{item.date}</td>
-      <td className="hidden md:table-cell">{item.startTime}</td>
-      <td className="hidden md:table-cell">{item.endTime}</td>
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/lessons/${item.id}`}>
@@ -73,7 +59,7 @@ const EventListPage = () => {
     <div className="bg-white rounded-md flex-1 p-4 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Announcements</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -92,11 +78,11 @@ const EventListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      <Table columns={columns} renderRow={renderRow} data={announcementsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default EventListPage;
+export default AnnouncementListPage;
