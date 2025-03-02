49a86ec605d1abe18e54c97b42b1da0b1e492e94
galangriarulhaq <galangrz337@gmail.com>
2025-03-02 22:16:31 +0700

    SetUp For Form Modal Desain

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 const FormModal = ({
   table,
@@ -16,11 +17,18 @@ const FormModal = ({
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor = type === "create" ? "bg-sYellow" : type === "update" ? "bg-sSky" : "bg-sPurple";
 
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`}>
+      <button className={`${size} flex items-center justify-center rounded-full ${bgColor}`} onClick={() => setOpen(true)}>
         <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center ">
+          <div className="bg-white rounded-md p-4">Galang</div>
+        </div>
+      )}
     </>
   );
 };
