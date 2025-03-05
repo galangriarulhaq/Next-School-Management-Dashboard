be407f1d99a405946f3a34ed41b96caae6a71dba
galangriarulhaq <galangrz337@gmail.com>
2025-03-05 21:09:29 +0700

    Update Form Modal

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -12,11 +12,16 @@ const StudentForm = dynamic(() => import("./forms/StudentForm"), {
   loading: () => <h1>Loading...</h1>,
 });
 
+const ParentForm = dynamic(() => import("./forms/ParentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
 const form: {
   [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
   student: (type, data) => <StudentForm type={type} data={data} />,
+  parent: (type, data) => <ParentForm type={type} data={data} />,
 };
 
 const FormModal = ({
