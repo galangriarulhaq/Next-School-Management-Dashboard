0c7bedb256dbed2629f4f7469217fb7b7fde8d81
galangriarulhaq <galangrz337@gmail.com>
2025-03-09 23:36:16 +0700

    Update Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,22 +1,38 @@
+"use client";
+
 import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/navigation";
 
 const Pagination = ({ page, count }: { page: number; count: number }) => {
+  const router = useRouter();
+
+  const hashPrev = ITEM_PER_PAGE * (page - 1) > 0;
+  const hashNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
+
+  const changePage = (nemPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", nemPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
+
   return (
     <div className="flex items-center p-4 justify-between text-gray-500">
-      <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+      <button disabled={!hashPrev} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed" onClick={() => changePage(page - 1)}>
         Prev
       </button>
       <div className="flex items-center gap-2 text-sm">
         {Array.from({ length: Math.ceil(count / ITEM_PER_PAGE) }, (_, index) => {
           const pageIndex = index + 1;
           return (
-            <button key={pageIndex} className="px-2 rounded-md bg-sSky">
+            <button key={pageIndex} className={`px-2 rounded-md ${pageIndex === page ? "bg-sSky" : ""}`} onClick={() => changePage(pageIndex)}>
               {pageIndex}
             </button>
           );
         })}
       </div>
-      <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Next</button>
+      <button disabled={!hashNext} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed" onClick={() => changePage(page + 1)}>
+        Next
+      </button>
     </div>
   );
 };
